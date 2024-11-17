a4f5026ce5a44ab80f000f45a39aec90d5516274
mohyd2233 <mhydersiddiqui0808@gmail.com>
2024-11-17 14:09:56 +0530

    finalcommit

--- app.py
+++ app.py
@@ -1,27 +1,16 @@
 from flask import Flask, render_template, jsonify, request
-import pickle
-import psycopg2  # PostgreSQL library for Python
-import os
+import pickle as pkl
+import sqlite3 as sql
 
 app = Flask(__name__)
-
-# Use your PostgreSQL connection URL from Railway
-DATABASE_URL = "postgresql://postgres:sbDVQXkpAdraxnoPUXMKouKmzUoyPmNZ@junction.proxy.rlwy.net:44338/railway"
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-# Function to create a new database connection
-def get_db_connection():
-    conn = psycopg2.connect(DATABASE_URL)
-    return conn
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/prediction', methods=['GET', 'POST'])
+@app.route('/prediction',methods = ['GET','POST'])
 def prediction():
-    if request.method == 'POST':
-        # Get input data from the form
+    if request.method=='POST':
         nitro = request.form.get('nitrogen')
         phos = request.form.get('phosphorus')
         pott = request.form.get('potassium')
@@ -29,60 +18,40 @@ def prediction():
         hum = request.form.get('humidity')
         ph = request.form.get('ph')
         rf = request.form.get('rainfall')
-        print(nitro, phos, pott, temp, hum, ph, rf)
-
-        # Load the machine learning model
-        with open('model.pkl', 'rb') as model_file:
-            mlmodel = pickle.load(model_file)
-
-        # Make a prediction
-        result = mlmodel.predict([[float(nitro), float(phos), float(pott), float(temp), float(hum), float(ph), float(rf)]])
-        
-        # Connect to PostgreSQL database and insert data
-        conn = get_db_connection()
+        print(nitro,phos,pott,temp,hum,ph,rf)
+        with open('model.pkl','rb') as model_file:
+            mlmodel = pkl.load(model_file)
+        res = mlmodel.predict([[float(nitro),float(phos),float(pott),float(temp),float(hum),float(ph),float(rf)]])
+        conn = sql.connect('cropdata.db')
         cur = conn.cursor()
-        cur.execute('''INSERT INTO CROP (Nitrogen, Phosphorus, Potassium, Temperature, Humidity, Ph, Rainfall, Result) 
-                       VALUES (%s, %s, %s, %s, %s, %s, %s, %s)''', 
-                    (nitro, phos, pott, temp, hum, ph, rf, result[0]))
-        
-        # Commit and close the connection
+        cur.execute(f''' INSERT INTO CROP VALUES({nitro},{phos},{pott},{temp},{hum},{ph},{rf},'{res[0]}')''')
         conn.commit()
-        cur.close()
-        conn.close()
-        
-        # Display the result on the result page
-        return render_template('result.html', result=result[0])
+        return render_template("result.html",res=res[0])
     else:
-        return render_template('prediction.html')
+        return render_template('/prediction.html')
 
-@app.route('/showdata', methods=['GET', 'POST'])
+
+@app.route('/showdata',methods = ['GET','POST'])
 def showdata():
-    # Connect to PostgreSQL database and retrieve data
-    conn = get_db_connection()
+    conn = sql.connect('cropdata.db')
     cur = conn.cursor()
-    
     cur.execute("SELECT * FROM CROP")
     x = cur.fetchall()
-    
-    cur.close()
-    conn.close()
 
-    # Format data for displaying
     lst = []
     for i in x:
         dict1 = {}
-        dict1['Nitrogen'] = i[0]
-        dict1['Phosphorus'] = i[1]
-        dict1['Potassium'] = i[2]
-        dict1['Temparature'] = i[3]
-        dict1['Humidity'] = i[4]
-        dict1['Ph'] = i[5]
-        dict1['Rainfall'] = i[6]
-        dict1['Result'] = i[7]
+        dict1['Nitrogen']=i[0]
+        dict1['Phosphorus']=i[1]
+        dict1['Potassium']=i[2]
+        dict1['Temparature']=i[3]
+        dict1['Humidity']=i[4]
+        dict1['Ph']=i[5]
+        dict1['Rainfall']=i[6]
+        dict1['Result']=i[7]
         lst.append(dict1)
 
-    # Show the data on the showdata page
-    return render_template('showdata.html', data=lst)
+    return render_template('showdata.html',data = lst)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5050)
+    app.run(host='0.0.0.0',port = 5050)
